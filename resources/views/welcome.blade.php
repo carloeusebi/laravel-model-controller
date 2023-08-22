9c502c2a8dd2537ae4d7572703aafb3539d7f004
carloeusebi <carloeusebi@gmail.com>
2023-08-22 14:54:29 +0200

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Larvel-model-controller</title>
+    <script defer type="module" src="{{ Vite::asset('resources/js/app.js') }}"></script>
+</head>
+
+<body>
+    <div class="container">
+        <h1 class="text-center mt-5">Hello world I'm movie db :)</h1>
+    </div>
+</body>
+
+</html>
